5f3408c4e112234b9fcc4d7d1d7b2c7e0b75e755
BobbyTheCatfish <46359040+BobbyTheCatfish@users.noreply.github.com>
2021-05-17 17:57:44 -0400

    Red Button

--- index.js
+++ index.js
@@ -1,74 +1,144 @@
 const { Plugin } = require('powercord/entities');
 const { getModule, React } = require('powercord/webpack');
-const { inject, uninject } = require('powercord/injector');
-const owoify = require('owoify-js').default;
-
+const { inject, uninject} = require('powercord/injector');
+const { findInReactTree } = require('powercord/util');
 const Settings = require('./components/Settings');
+const Button = require('./components/Button');
+const Button2 = require('./components/Button2.jsx')
+let owoifierAutoToggle = false;
 
-module.exports = class Owoify extends Plugin {
-    async startPlugin() {
-        powercord.api.settings.registerSettings(this.entityID, {
-            category: this.entityID,
-            label: 'Owoifier',
-            render: (props) => React.createElement(Settings, {
-                main: this,
-                ...props
-            })
-        });
-        
-        let parentThis = this;
 
-        function owoifyText(v) {
-            var words = v.split(' ');
-            var output = '';            
-            const level = parentThis.settings.get('owoLevel','owo');
+module.exports = class Owoify extends Plugin { 
 
-            for (let index = 0; index < words.length; index++) {
-                const element = words[index];
-                if (!element.startsWith('<@') && !element.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g)) {
-                    let format_string = owoify(element, level)
-                        .replace(/`/g, '\\`')
-                        .replace(/\*/g, '\\*');
+  async startPlugin () {
+    this.addButton();
+    powercord.api.settings.registerSettings(this.entityID, {
+      category: this.entityID,
+      label: 'Owoifier',
+      render: (props) => React.createElement(Settings, {
+        main:this,
+        ...props
+      })
+    }); 
 
-                    output += format_string + ' '
-                } else {
-                    output += element + ' '
-                }
-            }
+    const getSetting = (setting, defaultValue) => this.settings.get(setting, defaultValue);
 
-            return output
+    function owoifyText(v) {
+      var words = v.split(' ');
+      var output = '';
+
+      let toggleFaces = getSetting('enableFaces');
+
+      for (let index = 0; index < words.length; index++) {
+        const element = words[index];
+        if (!element.startsWith('<@') && !element.startsWith('<#') && !element.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g)) {
+          let format_string = element
+          .replace(/(?:r|l)/g, "w")
+          .replace(/(?:R|L)/g, "W")
+          .replace(/(n)([aeiou])/gi, '$1y$2')
+          .replace(/ove/g, "uv")
+          .replace(/th/g, "ff");
+
+          if (toggleFaces) {
+            let faceList = getSetting('owoifierFaces');            
+            format_string = format_string.replace(/\!+/g, " " + faceList[Math.floor(Math.random() * faceList.length)].name + " ");            
+          }
+
+          output += format_string + ' '
+        } else {
+          output += element + ' '
         }
+      }
 
-        const messageEvents = await getModule(["sendMessage"]);
-        inject("owoifierSend", messageEvents, "sendMessage", function(args) {
-            if (parentThis.settings.get('owoEnabled', false) && !args[1].content.startsWith(powercord.api.commands.prefix)) {
-                let text = args[1].content;
-                text = owoifyText(text);
-                args[1].content = text;
-            }
-            return args;
-        }, true);
-
-        powercord.api.commands.registerCommand({
-            command: 'owo',
-            description: 'owoify your message',
-            usage: '{c} [ text ]',
-            executor: (args) => ({ send: true, result: owoifyText(args.join(' ')) })
-        });
-        powercord.api.commands.registerCommand({
-            command: 'toggleowo',
-            description: `owoify all of your messages`,
-            executor: () => {
-                let owoifierAutoToggle = this.settings.get('owoEnabled', false); 
-                this.settings.set('owoEnabled', !owoifierAutoToggle);
-            }
-        });
+      return output
     }
+    
+    const messageEvents = await getModule(["sendMessage"]);
+    inject("owoifierSend", messageEvents, "sendMessage", function(args) {
+      if(owoifierAutoToggle) {
+        let text = args[1].content;
+        text = owoifyText(text);
+        args[1].content = text;      
+      }      
+      return args;  
+    }, true);
 
-    pluginWillUnload() {
-        powercord.api.settings.unregisterSettings(this.entityID);
-        uninject("owoifierSend");
-        powercord.api.commands.unregisterCommand('toggleowo');
-        powercord.api.commands.unregisterCommand('owo');
-    }    
-};
+    powercord.api.commands.registerCommand({
+      command: 'owoify',
+      description: 'owoify your message',
+      usage: '{c} [ text ]',
+      executor: (args) => ({send: true, result: owoifyText(args.join(' '))})
+    });
+    powercord.api.commands.registerCommand({
+      command: 'owoifyauto',
+      description: `owoify all of your messages`,
+      executor: this.toggleAuto.bind(this)
+    });    
+  }
+
+  addButton() {    
+    const ChannelTextAreaContainer = getModule(
+        m =>
+            m.type &&
+            m.type.render &&
+            m.type.render.displayName === "ChannelTextAreaContainer",
+        false
+    );
+    inject(
+        "owoifierButton",
+        ChannelTextAreaContainer.type,
+        "render",
+        (args, res) => {
+            const props = findInReactTree(
+                res,
+                r => r && r.className && r.className.indexOf("buttons-") === 0
+            );
+        
+            const element = React.createElement(
+                "div",
+                {
+                    key: owoifierAutoToggle,
+                    className: ".owoifierButton",
+                    onClick: () => {
+                      this.toggleAuto()
+                    },
+                },
+                React.createElement(owoifierAutoToggle == true ? Button2 : Button)
+            );
+            
+            const btnEnb = this.settings.get("buttonEnabled", true);
+            const posSetting = this.settings.get("buttonPos", false);
+            if(btnEnb) {posSetting ? props.children.push(element) : props.children.unshift(element);}
+            return res;
+        }
+    );
+    ChannelTextAreaContainer.type.render.displayName = "ChannelTextAreaContainer";
+}
+
+  pluginWillUnload() {
+    powercord.api.settings.unregisterSettings(this.entityID);
+    uninject("owoifierSend"); 
+    powercord.api.commands.unregisterCommand('owoifyauto');   
+    powercord.api.commands.unregisterCommand('owoify');   
+    uninject("owoifierButton");
+    const button = document.querySelector('.owoifierButton');
+    if(button) button.remove();
+  }
+
+  toggleAuto () {
+    owoifierAutoToggle = !owoifierAutoToggle;
+    //powercord.api.notices.sendToast('owoifyNotif', {
+      //header: `Owoify Auto Status`,
+      //content: `${(owoifierAutoToggle == true) ? 'Its weady to go >w<' : 'Ok chill we are back to normal again.'}`,
+      //timeout: 1000
+    //});
+    /*
+    const button = document.querySelector('.owoifierButton')
+    if(button){
+      uninject('owoifierButton')
+      button.remove()
+      this.addButton()
+
+    }*/
+  }
+}
